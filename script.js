cd1b5c020850fd0e3518ae2d1f627aedd06dc9a0
sr5392 <61883265+sr5392@users.noreply.github.com>
2023-06-17 23:39:00 +0200

    Implement function "playRound"

--- script.js
+++ script.js
@@ -19,3 +19,54 @@ function getComputerChoice() {
             break;
     }
 }
+
+function playRound(playerChoice, computerChoice) {
+
+    playerChoice = playerChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
+
+    if (playerChoice === "ROCK" && computerChoice === "ROCK") {
+        return "Draw";
+    }
+
+    if (playerChoice === "ROCK" && computerChoice === "PAPER") {
+        return "Lose";
+    }
+
+    if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
+        return "Win";
+    }
+
+
+
+    if (playerChoice === "PAPER" && computerChoice === "ROCK") {
+        return "Win";
+    }
+
+    if (playerChoice === "PAPER" && computerChoice === "PAPER") {
+        return "Draw";
+    }
+
+    if (playerChoice === "PAPER" && computerChoice === "SCISSORS") {
+        return "Lose";
+    }
+
+
+
+    if (playerChoice === "SCISSORS" && computerChoice === "ROCK") {
+        return "Lose";
+    }
+
+    if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
+        return "Win";
+    }
+
+    if (playerChoice === "SCISSORS" && computerChoice === "SCISSORS") {
+        return "Draw";
+    }
+}
+
+const playerChoice = "Rock";
+const computerChoice = getComputerChoice();
+
+console.log(playRound(playerChoice, computerChoice));
